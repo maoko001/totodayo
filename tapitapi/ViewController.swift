7bdc4849e60bbfef85229f64416e5f48615545f2
maoko <maomaofu0121@docomo.ne.jp>
2020-03-06 19:49:57 +0900

    Initial Commit

---
+++ tapitapi/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tapitapi
+//
+//  Created by Maoko Furuya on 2020/02/28.
+//  Copyright © 2020 Maoko Furuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
